26825eaa04c2b5395a66d5d6c0bf935c7a5d14ac
toshikatsu_asai <strategicrhythm@gmail.com>
2016-10-13 10:07:33 +0000

    show html

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,9 @@ Rails.application.routes.draw do
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
 
-  resources :users
+  resources :users do
+    collection do
+      post 'confirm'
+    end
+  end
 end
