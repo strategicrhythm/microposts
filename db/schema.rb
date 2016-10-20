96d3a70c221f743f9caa25503627f879230636fc
toshikatsu_asai <strategicrhythm@gmail.com>
2016-10-20 10:32:19 +0000

    make it better

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161014060130) do
+ActiveRecord::Schema.define(version: 20161019120350) do
+
+  create_table "bookmarks", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "micropost_id"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+  end
+
+  add_index "bookmarks", ["micropost_id"], name: "index_bookmarks_on_micropost_id"
+  add_index "bookmarks", ["user_id"], name: "index_bookmarks_on_user_id"
 
   create_table "microposts", force: :cascade do |t|
     t.integer  "user_id"
